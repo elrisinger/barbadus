6b37b15d29fc91b278e84f1abe68145ee5f8ffba
elrisinger@hotmail.com <>
2017-04-04 21:48:49 -0300

    Interface de Agendamento do Usuário

--- src/BarbadusBundle/Controller/DefaultController.php
+++ src/BarbadusBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace BarbadusBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
     {
@@ -17,20 +18,28 @@ class DefaultController extends Controller
         $servicos = $em->getRepository('BarbadusBundle:Servico')
                 ->findBy(array(), array('nome' => 'ASC'));
         
+        
+        $dtInicio = new \DateTime("+1 Day");
+        $dtFim = new \DateTime("+11 Day");
+        $intervalo = new \Dateinterval("P1D");  
+        $periodo = new \DatePeriod($dtInicio, $intervalo, $dtFim);        
+        
         return $this->render('BarbadusBundle:Default:index.html.twig', array(
-            'servicos' => $servicos
+            'servicos' => $servicos,
+            'datas' => $periodo
         ));
     }
     
     /**
      * @Route("/profissionais")
      */
-    public function profissionaisAction()
-    {        
+    public function profissionaisAction(Request $request)
+    {   
+        $idServico = $request->get("servico");
         $em = $this->getDoctrine()->getManager();
         
         $barbeiros = $em->getRepository('BarbadusBundle:Barbeiro')
-                ->findBy(array(), array('nome' => 'ASC'));
+                ->findBy(array("servico" => $idServico), array('nome' => 'ASC'));
         
         
         // forma opcional de passar Json
@@ -43,5 +52,30 @@ class DefaultController extends Controller
         return $this->json($barbeiros);
         
         
+    }
+    
+    /**
+     * @Route("/horarios")
+     */
+    public function horariosAction(Request $request)
+    {
+        $dtSelecionada = $request->get('dia');
+        
+        $dtInicio = new \DateTime($dtSelecionada);
+        $dtInicio->setTime(9,  0, 0);
+        $dtFim = new \DateTime($dtSelecionada);
+        $dtFim->setTime(18,  0, 0);
+        
+        $intervalo = new \Dateinterval("PT30M");  
+        $periodo = new \DatePeriod($dtInicio, $intervalo, $dtFim);
+        
+        foreach ($periodo as $dia)
+        {
+            $dias["hora"] = $dia->format('H:i');
+            $dias["disponivel"] = true;
+            $listaHorarios[] = $dias;
+        }
+        
+        return $this->json($listaHorarios);
     }
 }
--- web/js/agendamento.js
+++ web/js/agendamento.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     
     var navListItems = $('ul.setup-panel li a'),
@@ -19,7 +20,28 @@ $(document).ready(function() {
         }
     });
     
-    carregaBarbeiros();
+    $('#sel-servico').change(function(){        
+        
+        $('#activate-step-2').prop('disabled', 'disabled');
+        
+        if ($(this).val() > 0)
+        {
+            $('#activate-step-2').removeAttr('disabled');
+            
+        }
+        
+        carregaBarbeiros($(this).val());
+        
+    });
+    
+    $("#profissionais").on("click", "input[name=profSelecionado]", function(){
+        $('#activate-step-3').removeAttr('disabled');
+    });
+    
+    $("#sel-dia").change(function(){
+        geraHorarios($(this).val());
+        
+    });
     
     $('ul.setup-panel li.active a').trigger('click');
     
@@ -28,20 +50,48 @@ $(document).ready(function() {
         $('ul.setup-panel li:eq(1)').removeClass('disabled');
         $('ul.setup-panel li a[href="#step-2"]').trigger('click');
         $(this).remove();
-    })    
+    });
+    
+    $('#activate-step-3').on('click', function(e) {
+        $('ul.setup-panel li:eq(2)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+        $(this).remove();
+    });
+    
 });
 
-function carregaBarbeiros()
+function carregaBarbeiros(idServico)
 {
-    $.getJSON("/profissionais", function(retorno){
+    $.getJSON("/profissionais?servico="+idServico, function(retorno){
+        
+        $('#profissionais').empty();
+        
         retorno.forEach(function(el){
             var radio = '<div class="radio">'
-                    + '<label>'
-                    + '<input type="radio" name="profSelecionado" value="'+el.i+'">'
-                    + '</label>'
-                + '</div>'
-            $('#profissionais').append(radio);            
-        });        
+                    +'<label>'
+                    +  '<input type="radio" name="profSelecionado" value="'+el.id+'" >'
+                    + el.nome
+                    +'</label>'
+                +'</div>';
         
+            $('#profissionais').append(radio);
+        });
+                        
     });
+                
+}
+
+function geraHorarios(dia)
+{
+    $.getJSON("/horarios?dia="+dia, function(retorno){
+        
+       $('#horarios').empty();
+       retorno.forEach(function(el){
+           var radio = '<div class="radio">'
+                      +'<label><input type="radio" name="horario" value="'+el.hora+'" />'+el.hora+'</label>'
+                      +'</div>';
+              $('#horarios').append(radio);
+       });
+    });
+    
 }
